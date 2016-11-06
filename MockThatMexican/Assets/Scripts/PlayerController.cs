afc4811b9e4f753bf3a8fc003c3d0d8e1aaa88ec
ttallerm <tiit.tallermaa@hotmail.com>
2016-11-06 02:15:58 +0200

    initial setup

---
+++ MockThatMexican/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	NavMeshAgent navAgent;
+
+	void Awake()
+	{
+		navAgent = GetComponent<NavMeshAgent>();
+	}
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Move ();
+	}
+
+	void Move()
+	{        
+		if (Input.GetMouseButton (0)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out hit, 100)) {
+				navAgent.SetDestination (hit.point);                
+			}           
+		}
+	}
+}
